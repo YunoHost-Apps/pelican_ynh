47f073bb231504ab86bb9cad683aa48708b9d818
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-05-29 12:55:45 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="python3-pip python3-virtualenv python3-dev libldap2-dev libsasl2-dev libssl-dev python3-venv"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
